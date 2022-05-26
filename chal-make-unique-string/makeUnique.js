5c57a9ab82848b9cdb07a9cb9c812c268b733c26
Logan Leatherow <lleatherow@niagarawater.com>
2022-05-25 20:03:45 -0500

    makeUnique done

--- chal-make-unique-string/makeUnique.js
+++ chal-make-unique-string/makeUnique.js
@@ -1,1 +1,15 @@
 // Write your solution below:
+// console.log("hello")
+
+let makeUnique = (letterString) =>{
+    let newString =""
+    for (let i = 0; i < letterString.length; i++) {
+    if (newString.includes(letterString[i])) {
+    } else {
+        newString += letterString[i]
+        }
+    }
+    return newString
+}
+
+console.log(makeUnique("hello World"))
